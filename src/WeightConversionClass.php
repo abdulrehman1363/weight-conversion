66fd1b98f2d018543eb56a738d616755cd83db36
Abdul Rehman Aziz <42958150+abdulrehman1363@users.noreply.github.com>
2022-02-14 00:10:11 +0500

    add conversions
    add conversions

--- src/WeightConversionClass.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace QTech\WeightConversion;
-
-class WeightConversionClass
-{
-}
---
+++ tests/WeightTest.php
@@ -0,0 +1,8 @@
+<?php
+
+use QTech\WeightConversion\Weight;
+
+it('can convert kilograms to pounds', function () {
+   $pounds = Weight::kilograms(100)->toPounds();
+    expect($pounds)->toEqual(220.46);
+});
