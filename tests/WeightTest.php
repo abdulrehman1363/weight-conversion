1132d7482d09c9b1225b8f438e520726c545e4d4
Abdul Rehman Aziz <42958150+abdulrehman1363@users.noreply.github.com>
2022-02-14 20:11:37 +0500

    add stones
    add stones

--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -6,3 +6,8 @@ it('can convert kilograms to pounds', function () {
     $pounds = Weight::kilograms(100)->toPounds();
     expect($pounds)->toEqual(220.46);
 });
+
+it('can convert kilograms to stones', function () {
+    $pounds = Weight::kilograms(100)->toStones();
+    expect($pounds)->toEqual(15.747);
+});
